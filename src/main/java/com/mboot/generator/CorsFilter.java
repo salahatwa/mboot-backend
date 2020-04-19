f1cd2363d61d7afb3eecadc689c71c380503a0fd
salahatwa <salahsayedatwa@gmail.com>
2020-04-19 22:08:02 +0300

    update cors config

--- src/main/java/com/mboot/generator/CorsFilter.java
+++ src/main/java/com/mboot/generator/CorsFilter.java
@@ -1,40 +1,16 @@
 package com.mboot.generator;
 
-import java.io.IOException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class CorsFilter implements javax.servlet.Filter {
+@Configuration
+@EnableWebMvc
+public class CorsFilter implements WebMvcConfigurer {
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletResponse res = (HttpServletResponse) response;
-		HttpServletRequest req = (HttpServletRequest) request;
-
-		res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-		res.setHeader("Access-Control-Allow-Headers",
-				"Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, Connection, User-Agent, authorization, sw-useragent, sw-version");
-
-		// Just REPLY OK if request method is OPTIONS for CORS (pre-flight)
-//		if (req.getMethod().equals("OPTIONS")) {
-//			res.setStatus(HttpServletResponse.SC_OK);
-//			return;
-//		}
-		chain.doFilter(request, response);
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH");
 	}
 
-	@Override
-	public void destroy() {
-	}
-
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-	}
 }
