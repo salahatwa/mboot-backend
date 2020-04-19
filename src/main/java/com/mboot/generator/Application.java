df7f1217c910c8a4f3a52104854f671de85eaaae
salahatwa <salahsayedatwa@gmail.com>
2020-04-19 22:00:41 +0300

    Handle cors issue

--- src/main/java/com/mboot/generator/Application.java
+++ src/main/java/com/mboot/generator/Application.java
@@ -1,9 +1,15 @@
 package com.mboot.generator;
 
+import static java.util.Arrays.asList;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,5 +25,24 @@ public class Application implements ApplicationRunner{
 	public void run(ApplicationArguments args) throws Exception {
 		log.info("Generator started");
 	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		final CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(asList("*"));
+		configuration.setAllowedMethods(asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+		// setAllowCredentials(true) is important, otherwise:
+		// The value of the 'Access-Control-Allow-Origin' header in the response must
+		// not be the wildcard '*' when the request's credentials mode is 'include'.
+		configuration.setAllowCredentials(true);
+		// setAllowedHeaders is important! Without it, OPTIONS preflight request
+		// will fail with 403 Invalid CORS request
+		configuration.setAllowedHeaders(asList("Authorization", "Cache-Control", "Content-Type", "x-requested-with",
+				"authorization", "credential", "X-XSRF-TOKEN"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 
 }
